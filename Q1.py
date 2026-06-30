28b810e7175b7c04d1026c17ef54823ad9abb22c
Vedant Dange <vedantdange35@gmail.com>
2026-06-30 12:51:30 +0530

    Basic Array Creation

---
+++ Q1.py
@@ -0,0 +1,21 @@
+#Q.1)
+import numpy as np
+# a) Create a 1D array
+arr1 = np.array([10, 20, 30, 40, 50])
+# b) Create a 3x3 2D array
+arr2 = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+# Print 1D array
+print("1D Array:")
+print(arr1)
+print("Shape:", arr1.shape)
+print("Data Type:", arr1.dtype)
+print()
+# Print 2D array
+print("2D Array:")
+print(arr2)
+print("Shape:", arr2.shape)
+print("Data Type:", arr2.dtype)
