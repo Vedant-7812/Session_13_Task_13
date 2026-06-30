cf621db7df4e6511cc58fa68591942077158ee27
Vedant Dange <vedantdange35@gmail.com>
2026-06-30 12:55:15 +0530

    Implementation of np.zeros() and np.ones()

---
+++ Q2.py
@@ -0,0 +1,15 @@
+#Q.2)
+import numpy as np
+# a) 1D array of 8 zeros
+arr1 = np.zeros(8)
+# b) 2D array of shape (4,4) filled with ones
+arr2 = np.ones((4, 4))
+# c) 3x3 matrix of zeros
+arr3 = np.zeros((3, 3))
+# Print all arrays
+print("1D Array of 8 Zeros:")
+print(arr1)
+print("\n4x4 Array of Ones:")
+print(arr2)
+print("\n3x3 Matrix of Zeros:")
+print(arr3)
